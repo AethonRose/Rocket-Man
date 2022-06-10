c29c80015606b980bd8a08a7c8e6cd88900cebfb
AethonRose <bobbysteiskal8565@gmail.com>
2022-06-10 06:07:17 -0700

    Load Next Level (46)

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,7 +8,7 @@ public class CollisionHandler : MonoBehaviour
     void OnCollisionEnter(Collision other) 
     {
         // Switch on other.gameObject.tag
-        switch(other.gameObject.tag)
+        switch (other.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("Friendly");
@@ -17,7 +17,7 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Fuel");
                 break;
             case "LandingPad":
-                Debug.Log("LandingPad");
+                LoadNextLevel();
                 break;
             default:
                 ReloadSceneOnDeath();
@@ -25,11 +25,26 @@ public class CollisionHandler : MonoBehaviour
         }
     }
 
-    // Reload Level Method
+    // Reloads Level On Collision with Untagged objects
     void ReloadSceneOnDeath()
     {
         // Loads Scene with value of currentSceneIndex
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    // Loads Next Level on Collision with LandingPad
+    void LoadNextLevel()
+    {
+        // Set nextSceneIndex to currentScenebuildIndex + 1
+        int nextSceneIndex = (SceneManager.GetActiveScene().buildIndex + 1);
+
+        // If nextSceneIndex equals total sceneCountInBuildSetting set nextSceneIndex to 0
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        // LoadScene of nextSceneIndex
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
